15aa0a13d5e5e3b21aab991a1072a573acede766
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-08 00:00:56 -0400

    created 404 page route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('wassup')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
 const PORT = process.env.PORT
 
 app.listen(PORT, console.log(`listening on port ${PORT}`))
