8fcf1ce062d15bcb208428fed61761c4beeb2a2c
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-10 22:53:10 -0400

    added imgs and Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we cant find this page!</p>
+                <img src="/images/404error" alt="404 ERROR" />
+                <div>
+                    <a href="https://www.freepik.com/free-vector/error-404-concept-illustration_7741849.htm#page=2&query=404%20funny&position=37&from_view=keyword&track=ais">Image by storyset</a> on Freepik
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/street-food" alt="Street Food"/>
+                    <div>
+                    Image by <a href="https://pixabay.com/users/masashiwakui-4385858/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2014619">Masashi Wakui</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2014619">Pixabay</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
