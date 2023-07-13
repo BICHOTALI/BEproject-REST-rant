ab6e65b1aa079e7be301118cb17121b322a9bc81
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-12 23:03:11 -0400

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -28,6 +28,22 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  const id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } 
+  else if (!places[id]) {
+    res.render('error404')
+  } 
+  else {
+    res.render('places/edit', {
+      place: places[id]
+    })
+  }
+})
+
+
 // POST creates new place
 router.post('/', (req, res) => {
   if (!req.body.pic) {
