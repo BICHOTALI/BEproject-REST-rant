684f7200d568604d733be93ea2d21bb4fdcf9149
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-12 20:56:58 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -14,21 +14,19 @@ router.get('/', (req, res) => {
 
 // POST creates new place
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('/places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
-//   if (!req.body.pic) {
-//     // Default image if one is not provided
-//     req.body.pic = 'http://placekitten.com/400/400'
-//   }
-//   if (!req.body.city) {
-//     req.body.city = 'Anytown'
-//   }
-//   if (!req.body.state) {
-//     req.body.state = 'USA'
-//   }
-//   places.push(req.body)
-//   res.redirect('/places')
-// })
+
 
 module.exports = router
