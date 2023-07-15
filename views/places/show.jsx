019fab5e8460f6f361c40a05c107aefc9ed28f57
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-15 18:29:26 -0400

    updated show page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,12 +6,17 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt={data.place.pic} />
             <div>
                 <h2>Rating</h2>
                 <p>Not Rated</p>
             </div>
             <div>
                 <h2>Description</h2>
+                <p>Located in {data.place.city},{data.place.state} and serves {data.place.cuisines}</p>
+            </div>
+            <div>
+                <h2>Comments</h2>
                 <p>No comments yet!</p>
             </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
