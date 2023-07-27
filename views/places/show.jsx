e68ff2c1266f1cb3d84db5f0adc39bd153e371b2
ALISON SALINAS <gisellsalinas43@gmail.com>
2023-07-27 19:03:54 -0400

    Display stars for ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -16,16 +16,20 @@ function show (data) {
       let sumRatings = data.place.comments.reduce((tot, c) => {
         return tot + c.stars
       }, 0)
-      let averageRating = sumRatings / data.place.comments.length
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐️'
+      }
       rating = (
         <h3>
-          {Math.round(averageRating)} stars
+          {stars} 
         </h3>
       )
         comments = data.place.comments.map(c => {
             return (
                 <div className='border'>
-                    <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+                    <h2 className='rant'>{c.rant ? 'Rant 🤬' : 'Rave 🥰'}</h2>
                     <h4>{c.content}</h4>
                     <h3>
                         <strong>- {c.author}</strong>
